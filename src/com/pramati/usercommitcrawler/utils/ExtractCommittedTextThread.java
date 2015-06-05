2931d6f72a6c3a4f8f3d785cfff8392f6c1f35c3
talib-pramati <talib.ahmad@imaginea.com>
2015-06-05 18:28:35 +0530

    Adding new Class.

---
+++ src/com/pramati/usercommitcrawler/utils/ExtractCommittedTextThread.java
@@ -0,0 +1,119 @@
+package com.pramati.usercommitcrawler.utils;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+
+import org.joda.time.DateTime;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.pramati.usercommitcrawler.beans.RepositoryCommitHistory;
+import com.pramati.usercommitcrawler.constants.UserCommitCrawlerConstants;
+import com.sun.istack.internal.logging.Logger;
+
+public class ExtractCommittedTextThread implements Callable<RepositoryCommitHistory>{
+
+	
+	 String repositoryCommitHistoryPage;
+	 List<String> pagesContainingCommittedText;
+	 
+	 public static final Logger LOGGER = Logger.getLogger(ExtractCommittedTextThread.class);
+	ExtractCommittedTextThread(String repositoryCommitHistoryPage,List<String> pagesContainingCommittedText)
+	{
+		this.repositoryCommitHistoryPage = repositoryCommitHistoryPage;
+		this.pagesContainingCommittedText = pagesContainingCommittedText; 
+	}
+	
+	@Override
+	public RepositoryCommitHistory call() throws Exception {
+		
+		RepositoryCommitHistory repositoryCommitHistory = new RepositoryCommitHistory();
+		repositoryCommitHistory.setRepoSitoryName(repositoryCommitHistoryPage.split("/")[4]);
+		
+		for (String pageContainingCommittedText : pagesContainingCommittedText) {
+			Document documnent = Jsoup.connect(pageContainingCommittedText)
+					.get();
+			String date = documnent.select("time").attr("datetime");
+
+			DateTime committedPagesdDateTime = new DateTime(date);
+			DateTime today = DateTime.now();
+			DateTime yesterday = DateTime.now().minusDays(1);
+
+			if (committedPagesdDateTime.getYear() == today.getYear()
+					&& committedPagesdDateTime.getMonthOfYear() == today
+							.getMonthOfYear()
+					&& committedPagesdDateTime.getDayOfMonth() == today
+							.getDayOfMonth()) {
+				appendCommitText(repositoryCommitHistory,
+						CommitHistoryFields.TODAY,
+						pageContainingCommittedText);
+			}
+
+			else if (committedPagesdDateTime.getYear() == yesterday
+					.getYear()
+					&& committedPagesdDateTime.getMonthOfYear() == yesterday
+							.getMonthOfYear()
+					&& committedPagesdDateTime.getDayOfMonth() == yesterday
+							.getDayOfMonth()) {
+				appendCommitText(repositoryCommitHistory,
+						CommitHistoryFields.YESTERDAY,
+						pageContainingCommittedText);
+			}
+
+			else {
+
+				appendCommitText(repositoryCommitHistory,
+						CommitHistoryFields.DAYS_BEFORE_YESTERDAY,
+						pageContainingCommittedText);
+			}
+		}
+		
+		return repositoryCommitHistory;
+	}
+	
+	public void appendCommitText(
+			RepositoryCommitHistory repositoryCommitHistory,
+			CommitHistoryFields fieldValue, String url) throws IOException {
+
+		try {
+			Document document = Jsoup.connect(url).get();
+			Elements commitTexts = document
+					.select(UserCommitCrawlerConstants.TEXT_SELECTOR_REGEX);
+			StringBuilder commitTextStringBuilder = new StringBuilder();
+			for (Element committedText : commitTexts) {
+				String text = committedText.text();
+				text = text.replace("+", "").replace("<", "<'")
+						.replace(";", "<BR>");
+				commitTextStringBuilder.append(text);
+			}
+
+			switch (fieldValue) {
+			case YESTERDAY:
+				repositoryCommitHistory.getYesterdaysCommit().append(
+						commitTextStringBuilder);
+				break;
+			case TODAY:
+				repositoryCommitHistory.getTodaysCommit().append(
+						commitTextStringBuilder);
+				break;
+			case DAYS_BEFORE_YESTERDAY:
+				repositoryCommitHistory.getDayBeforeYesrdaysCommit().append(
+						commitTextStringBuilder);
+				break;
+			}
+
+		} catch (SocketTimeoutException exception) {
+			if (LOGGER.isLoggable(Level.WARNING)) {
+				LOGGER.log(Level.WARNING,
+						"SocketTimeOut Exception has occured. ");
+			}
+		}
+
+	}
+
+}
